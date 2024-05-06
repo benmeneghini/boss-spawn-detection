a84eeaad6b531eab28667f818413cc33ec259ef2
Ben Meneghini <bencmeneghini@gmail.com>
2024-05-06 21:07:34 +1200

    Added training and validation images (data)

--- model_training.py
+++ model_training.py
@@ -6,7 +6,5 @@ def train_model():
     # Load a model
     model = YOLO('yolov8n.pt')  # load a pretrained model (recommended for training)
 
-    # Train the model with 2 GPUs ???
-
     # Can add augmentation parameters here to improve model accuracy
     results = model.train(data='boss_v8.yaml', imgsz=640, epochs=20, device='mps', name='yolov8n_custom')
